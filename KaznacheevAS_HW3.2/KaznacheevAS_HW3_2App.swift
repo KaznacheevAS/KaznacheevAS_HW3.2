68fd21e9972dca93e9a30b028547ac3d4f876f90
Kaznacheev <antkaznacheev@gmail.ru>
2021-09-13 18:10:15 +0300

    Initial Commit

---
+++ KaznacheevAS_HW3.2/KaznacheevAS_HW3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  KaznacheevAS_HW3_2App.swift
+//  KaznacheevAS_HW3.2
+//
+//  Created by Kaznacheev on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct KaznacheevAS_HW3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
